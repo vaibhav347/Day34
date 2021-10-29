0acb02193ebfa95121dd77aa53d0f0aaf62f6a27
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-29 11:05:07 +0530

    File inserted Successfully

---
+++ DBConnection/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBConnection
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int salary { get; set; }
+        public DateTime startDate { get; set; }
+        public string gender { get; set; }
+    }
+}
