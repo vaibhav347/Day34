b8bb23c4d7c393b613d12d29c2e9a3265e508e2d
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-29 11:11:07 +0530

    File inserted Successfully

---
+++ AddressBookDay34/ConnectDB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DBConnection
+{
+    public class ConnectDB
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PayrollService;Integrated Security=True";
+        SqlConnection conn = new SqlConnection(connectionString);
+        public void getData()
+        {
+            try
+            {
+                Employee emp = new Employee();
+                using(this.conn)
+                {
+                    string query = @"select * from addressBook";
+                    SqlCommand cmd = new SqlCommand(query, this.conn);
+                    this.conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if(dr.HasRows)
+                    {
+                        while(dr.Read())
+                        {
+                            emp.id = dr.GetInt32(0);
+                            emp.name = dr.GetString(1);
+                            emp.contact = dr.GetInt32(2);
+                            emp.city = dr.GetString(3);
+                            emp.gender = dr.GetString(4);
+                            Console.WriteLine("{0},{1},{2},{3},{4}",emp.id, emp.name,emp.contact,emp.city,emp.gender);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                    dr.Close();
+                    this.conn.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
---
+++ AddressBookDay34/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBConnection
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int contact { get; set; }
+        public string city { get; set; }
+        public string gender { get; set; }
+    }
+}
